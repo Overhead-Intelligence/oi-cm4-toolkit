996e7504898e16c5f9bbbf4f7e0a66e62f8f9b5e
Jean Bezerra <jean@overheadintel.com>
2024-09-23 21:15:50 +0000

    Parameter change

--- wwan0-setup.sh
+++ wwan0-setup.sh
@@ -8,7 +8,7 @@ sudo ip link set wwan0 up
 
 sudo qmicli -d /dev/cdc-wdm0 --dms-set-operating-mode='online'
 
-sudo qmicli -p -d /dev/cdc-wdm0 --device-open-net='net-raw-ip|net-no-qos-header' --wds-start-network="apn='vzwinternet',username='ZAXISSOLUTIONS',password='cJ6j_Q+48z?HZt',ip-type=4" --client-no-release-cid
+sudo qmicli -p -d /dev/cdc-wdm0 --device-open-net='net-raw-ip|net-no-qos-header' --wds-start-network="apn='vzwinternet',ip-type=4" --client-no-release-cid
 
 sudo udhcpc -q -f -i wwan0
 
