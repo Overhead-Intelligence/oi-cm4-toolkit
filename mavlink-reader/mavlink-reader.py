474553b9e5acce48eb08aa4fed5968080c326091
Jean Bezerra <jean@overheadintel.com>
2025-03-03 21:54:41 +0000

    Add SYSTEM_TIME message parsing, add log_path argument to save mavlink-data to a specified path

--- mavlink-reader/mavlink-reader.py
+++ mavlink-reader/mavlink-reader.py
@@ -3,6 +3,8 @@ from pymavlink import mavutil
 import time
 import csv
 import fcntl
+from datetime import datetime
+import os
 
 # Constants
 UDP_IP = "127.0.0.1"
@@ -10,17 +12,21 @@ UDP_PORT = 10004
 
 class MavLinkData:
     def __init__(self):
-        self.armed = False,
-        self.rangefinder_dst = 0.0,
-        self.agl = 0.0,
-        self.battery = 0.0,
-        self.heading = 0,
-        self.flight_mode = "stabilize",
-        self.wind_dir = 0,
-        self.wind_speed = 0,
-        self.wind_speed_z = 0
+        self.armed = False
+        self.rangefinder_dst = 0.0
+        self.agl = 0.0
+        self.battery = 0.0
+        self.heading = 0.0
+        self.flight_mode = "default"
+        self.wind_dir = 0.0
+        self.wind_speed = 0.0
+        self.wind_speed_z = 0.0
+        self.ground_speed = 0.0
+        self.air_speed = 0.0
+        self.unix_time = 0.0
+        self.mavlink_log_filepath = "/home/droneman/"
     
-    def update_data(self, msg, armed=None, rangefinder_dst=None, agl=None, battery=None, heading=None, flight_mode=None, wind_dir=None, wind_speed = None, wind_speed_z = None):
+    def update_data(self, msg, armed=None, rangefinder_dst=None, agl=None, battery=None, heading=None, flight_mode=None, wind_dir=None, wind_speed = None, wind_speed_z = None, ground_speed = None, air_speed = None, unix_time = None):
         """
         Fills data which is present at time mavlink message read.
         
@@ -59,6 +65,8 @@ class MavLinkData:
                     flight_mode = "qloiter"
                 elif msg.custom_mode == 21:
                     flight_mode = "qrtl"
+                else:
+                    flight_mode = "unknown"
     
         elif msg and msg.get_type() == 'RANGEFINDER':
             rangefinder_dst = msg.distance  # Rangefinder in meters
@@ -68,10 +76,14 @@ class MavLinkData:
             battery = (msg.voltages[0]/1000)  # Battery voltage in Volts
         elif msg and msg.get_type() == 'VFR_HUD':
             heading = msg.heading # Heading in degrees
+            ground_speed = msg.groundspeed 
+            air_speed = msg.airspeed
         elif msg and msg.get_type() == 'WIND':
             wind_dir = msg.direction  # wind_dir
             wind_speed = msg.speed 
             wind_speed_z = msg.speed_z
+        elif msg and msg.get_type() == 'SYSTEM_TIME':
+            unix_time = msg.time_unix_usec # unix time in microseconds
              
         self.armed = armed if armed is not None else self.armed
         self.rangefinder_dst = rangefinder_dst if rangefinder_dst is not None else self.rangefinder_dst
@@ -82,28 +94,46 @@ class MavLinkData:
         self.wind_dir = wind_dir if wind_dir is not None else self.wind_dir
         self.wind_speed = wind_speed if wind_speed is not None else self.wind_speed
         self.wind_speed_z = wind_speed_z if wind_speed_z is not None else self.wind_speed_z
+        self.ground_speed = ground_speed if ground_speed is not None else self.ground_speed
+        self.air_speed = air_speed if air_speed is not None else self.air_speed
+        self.unix_time = unix_time if unix_time is not None else self.unix_time
     
     def write_to_csv(self):
-        file_path = "/home/droneman/shell-scripts/mavlink-reader/mavlink-data.csv"
+        default_file_path = "/home/droneman/shell-scripts/mavlink-reader/mavlink-data.csv"
+        mission_file_path = os.path.join(self.mavlink_log_filepath, "mavlink-data.csv")
+ 
         data = {
+            'flight_mode': self.flight_mode,
             'armed': self.armed,
+            'battery': self.battery,
             'rangefinder_dst': self.rangefinder_dst,
             'agl': self.agl,
-            'battery': self.battery,
             'heading': self.heading,
-            'flight_mode': self.flight_mode,
+            'ground_speed' : self.ground_speed,
+            'air_speed' : self.air_speed,
             'wind_dir': self.wind_dir,
             'wind_speed': self.wind_speed,
-            'wind_speed_z': self.wind_speed_z
+            #'wind_speed_z': self.wind_speed_z,
+            'UTC_Date_Time': datetime.utcfromtimestamp(self.unix_time / 1e6).strftime('%Y-%m-%d %H:%M:%S')  # Convert Unix time (in microseconds) to human-readable format (UTC)
         }
 
-        with open(file_path, mode='w', newline='') as file:
-            
+        with open(default_file_path, mode='w', newline='') as file:
             fcntl.flock(file, fcntl.LOCK_EX) #lock the file before we write to it
             writer = csv.DictWriter(file, fieldnames=data.keys())
             writer.writeheader()
             writer.writerow(data)
             fcntl.flock(file,fcntl.LOCK_UN) #unlock the file
+        
+        def append_row(file_path, data):
+            file_exists = os.path.exists(file_path)
+            mode = 'a' if file_exists else 'w'
+            with open(file_path, mode=mode, newline='') as file:
+                writer = csv.DictWriter(file, fieldnames=data.keys())
+                if not file_exists:
+                    writer.writeheader()
+                writer.writerow(data)
+
+        append_row(mission_file_path, data)
 
 class MavLinkReader:
     """
@@ -290,18 +320,27 @@ def main():
     """
     Main entry point for the script.
     """
-    # Check for correct number of arguments in function call
-    if len(sys.argv) != 2:
-        print("Usage: python read-mav.py { -command- }")
-        sys.exit(1)
-
-    # Parse command-line arguments
-    command = sys.argv[1].lower()
+    # For user command input
+    command = None
     
-    # Create an instance of MavLinkReader
+    # Create an instance of MavLinkReader and MavLinkData
     reader = MavLinkReader()
     data = MavLinkData()
 
+
+    # Check for correct number of arguments in function call
+    if len(sys.argv) > 3 or len(sys.argv) == 1:
+        print("Usage: python read-mav.py {command} {mavlink_log_filepath -optional-}")
+        sys.exit(1)
+
+    # Parse command-line arguments
+    if len(sys.argv) == 3:
+        command = sys.argv[1].lower() #command
+        data.mavlink_log_filepath = sys.argv[2] #log filepath
+    elif len(sys.argv) == 2:   
+        command = sys.argv[1].lower() #command
+    
+    
     if command != "stream":
         # Execute the specified command
         reader.execute_command(command)
@@ -309,7 +348,7 @@ def main():
     elif command == "stream":
         last_sent_time = time.time() #initialize time variable
 
-        message_types = ['HEARTBEAT', 'TERRAIN_REPORT', 'RANGEFINDER', 'BATTERY_STATUS', 'VFR_HUD', 'WIND'] 
+        message_types = ['HEARTBEAT', 'TERRAIN_REPORT', 'RANGEFINDER', 'BATTERY_STATUS', 'VFR_HUD', 'WIND', 'SYSTEM_TIME'] 
 
         while True:
             # read MAVLink messages
@@ -328,7 +367,7 @@ def main():
                     last_sent_time = current_time  # Update the last write time
             else:
                 time.sleep(0.05)
-
+    
 
 if __name__ == "__main__":
     main()
