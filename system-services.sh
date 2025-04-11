fc45ab23f561a59391c44cd0f959d8a282363fac
Jean <jean@overheadintel.com>
2025-04-11 15:11:53 +0000

    Add cot-broadcast to service check

--- system-services.sh
+++ system-services.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the services to check
-SERVICES=("mavlink-router" "set-datetime" "photogram" "quspin-mag" "mavlink-mag-forwarder")
+SERVICES=("mavlink-router" "set-datetime" "photogram" "quspin-mag" "mavlink-mag-forwarder" "cot-broadcast")
 
 # Function to check the status of services
 check_status() {
