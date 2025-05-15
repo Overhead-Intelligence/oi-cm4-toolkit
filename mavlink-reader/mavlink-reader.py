9b380e4da17fa25d4423dd0ba0b686d494844069
Jean Bezerra <jean@overheadintel.com>
2025-05-15 14:24:05 +0000

    Remove unused print  functions, add argument for udp port for multple instances of script

--- mavlink-reader/mavlink-reader.py
+++ mavlink-reader/mavlink-reader.py
@@ -8,9 +8,9 @@ import os
 
 # Constants
 UDP_IP = "127.0.0.1"
-UDP_PORT = 10003
+UDP_PORT = 10006
 
-DRONE_SYS_ID = 1 # The system ID of the drone we want to read data from, change this to match the drone's system ID
+DRONE_SYS_ID = 28 # The system ID of the drone we want to read data from, change this to match the drone's system ID
 
 class MavLinkData:
     def __init__(self):
@@ -126,13 +126,6 @@ class MavLinkData:
             'lon': self.lon,
         }
 
-        # Always write to this default filepath
-        with open(default_file_path, mode='w', newline='') as file:
-            fcntl.flock(file, fcntl.LOCK_EX) #lock the file before we write to it
-            writer = csv.DictWriter(file, fieldnames=data.keys())
-            writer.writeheader()
-            writer.writerow(data)
-            fcntl.flock(file,fcntl.LOCK_UN) #unlock the file
         
         # If the user has provided a mission log file path,
         # then also append the row to that file.
@@ -148,7 +141,15 @@ class MavLinkData:
                     writer.writerow(data)
 
             append_row(mission_file_path, data)
-
+        else:
+            # if no filepath is given, write to this default filepath
+            with open(default_file_path, mode='w', newline='') as file:
+                fcntl.flock(file, fcntl.LOCK_EX) #lock the file before we write to it
+                writer = csv.DictWriter(file, fieldnames=data.keys())
+                writer.writeheader()
+                writer.writerow(data)
+                fcntl.flock(file,fcntl.LOCK_UN) #unlock the file
+       
 class MavLinkReader:
     """
     A class to encapsulate MAVLink interactions and commands.
@@ -158,125 +159,7 @@ class MavLinkReader:
         """
         Initialize MAVLink connection.
         """
-        self.mav = mavutil.mavlink_connection(f'udp:{ip}:{port}')
-
-    def get_armed_status(self):
-        """
-        Check and return the armed status of the drone.
-
-        Returns:
-            bool: True if armed, False otherwise.
-        """
-        while True:
-            msg = self.mav.recv_msg()
-            #print(msg)
-            if msg and msg.get_type() == 'HEARTBEAT':
-                if msg.type == 1 and msg.autopilot == 3:  # Fixed-wing, ArduPilot
-                    return (msg.base_mode & mavutil.mavlink.MAV_MODE_FLAG_SAFETY_ARMED) != 0
-            time.sleep(0.1)
-
-    def get_agl_altitude(self):
-        """
-        Get the altitude above ground level (AGL).
-
-        Returns:
-            float: AGL altitude in meters, or None if unavailable.
-        """
-        while True:
-            msg = self.mav.recv_msg()
-            if msg and msg.get_type() == 'TERRAIN_REPORT':
-                return msg.current_height  # AGL Altitude in meters
-            time.sleep(0.1)
-
-    def get_rangefinder_distance(self):
-        """
-        Get the rangefinder value.
-
-        Returns:
-            float: Rangefinder value in meters, or None if unavailable.
-        """
-        while True:
-            msg = self.mav.recv_msg()
-            if msg and msg.get_type() == 'RANGEFINDER':
-                return msg.distance  # Rangefinder in meters
-            time.sleep(0.1)
-    
-    def get_battery_remaining(self):
-        """
-        Get the battery remaining value.
-
-        Returns:
-            float: Battery life in percentage value 0-100%.
-        """
-        while True:
-            msg = self.mav.recv_msg()
-            if msg and msg.get_type() == 'BATTERY_STATUS':
-                return (msg.voltages[0]/1000)  # Rangefinder in meters
-            time.sleep(0.1)
-
-    def get_flight_mode(self):
-        """
-        Get the battery remaining value.
-
-        Returns:
-            float: Battery life in percentage value 0-100%.
-        """
-        while True:
-            msg = self.mav.recv_msg()
-            #print(msg)
-            # !! These modes may NOT be accurage, testing still needed... !!
-            if msg and msg.get_type() == 'HEARTBEAT':
-                if msg.custom_mode == 0:
-                    return "stabilize"
-                elif msg.custom_mode == 3:
-                    return "auto"
-                elif msg.custom_mode == 4:
-                    return "guided"
-                elif msg.custom_mode == 5:
-                    return "loiter"
-                elif msg.custom_mode == 11:
-                    return "althold"
-
-            time.sleep(0.1)        
-
-    def execute_command(self, command):
-        """
-        Execute a specified command and print the result.
-
-        Args:
-            command (str): The command to execute.
-        """
-        # Available commands, dispatch dictionary
-        commands = {
-            "armed": lambda: self.get_armed_status(),
-            "agl": lambda: self.get_agl_altitude(),
-            "rangefinder": lambda: self.get_rangefinder_distance(),
-            "wind_dir" : lambda: self.get_wind(),
-            "battery" : lambda: self.get_battery_remaining(),
-            "flight_mode" : lambda: self.get_flight_mode(),
-        }
-
-        # Execute the corresponding function
-        if command in commands:
-            result = commands[command]() # call the function by accessing the value using key "command"
-            
-            # armed command returns an explicit 'true' or 'false' string
-            if command == "armed":
-                print("true" if result else "false")
-            
-            # everything else should return a float, we can handle other types as needed
-            elif command in ["agl","rangefinder", "wind", "battery"]:
-                if result is not None:
-                    print(result)
-                else:
-                    print("nan")
-                    sys.exit(2)
-
-        # Catch when an unknown command is passed
-        else:
-            print(f"Unknown command: {command}")
-            print("Supported commands: " + ", ".join(commands.keys()))
-            sys.exit(1)
+        self.mav = mavutil.mavlink_connection(f'udp:{ip}:{port}') 
 
 def main():
     """
@@ -284,30 +167,29 @@ def main():
     """
     # For user command input
     command = None
-    
+
     # Create an instance of MavLinkReader and MavLinkData
-    reader = MavLinkReader()
     data = MavLinkData()
 
-
     # Check for correct number of arguments in function call
-    if len(sys.argv) > 3 or len(sys.argv) == 1:
-        print("Usage: python read-mav.py {command} {mavlink_log_filepath -optional-}")
+    if len(sys.argv) > 4 or len(sys.argv) == 1:
+        print("Usage: python mavlink-reader.py {command} {mavlink_log_filepath udp_port -optional-}")
         sys.exit(1)
 
     # Parse command-line arguments
-    if len(sys.argv) == 3:
+    if len(sys.argv) >= 3:
         command = sys.argv[1].lower() #command
         data.mavlink_log_filepath = sys.argv[2] #log filepath
+        port = sys.argv[3] #port
+
     elif len(sys.argv) == 2:   
         command = sys.argv[1].lower() #command
-    
-    
-    if command != "stream":
-        # Execute the specified command
-        reader.execute_command(command)
+        port = UDP_PORT #default port
 
-    elif command == "stream":
+    reader = MavLinkReader(port=port) # Create an instance of MavLinkReader with the specified port
+    
+
+    if command == "stream":
         last_sent_time = time.time() #initialize time variable
 
         message_types = ['HEARTBEAT', 'TERRAIN_REPORT', 'RANGEFINDER', 'BATTERY_STATUS', 'VFR_HUD', 'WIND', 'SYSTEM_TIME', 'GLOBAL_POSITION_INT'] 
