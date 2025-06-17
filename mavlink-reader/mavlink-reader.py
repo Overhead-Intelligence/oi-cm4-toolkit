26b09fc12f3baac5f1f12b999715c55d7bb8f32e
Jean <jean@overheadintel.com>
2025-06-17 08:53:34 -0400

    Fix getSrcSystem(), function call

--- mavlink-reader/mavlink-reader.py
+++ mavlink-reader/mavlink-reader.py
@@ -10,7 +10,7 @@ import os
 UDP_IP = "127.0.0.1"
 UDP_PORT = 10006
 
-DRONE_SYS_ID = 28 # The system ID of the drone we want to read data from, change this to match the drone's system ID
+DRONE_SYS_ID = 23 # The system ID of the drone we want to read data from, change this to match the drone's system ID
 
 class MavLinkData:
     def __init__(self):
@@ -200,7 +200,7 @@ def main():
             #msg = reader.mav.recv_msg()
 
             # filter messages based on source system ID, we only want messages from this drone (DRONE_SYS_ID)
-            if not msg or msg.get_srcSystem != DRONE_SYS_ID:
+            if not msg or msg.get_srcSystem() != DRONE_SYS_ID:
                 continue
 
             #print(msg)
